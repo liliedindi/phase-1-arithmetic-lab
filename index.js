d97ffb28ac7826fabc6b6a60ff386db993c58d86
liliedindi <liliandindinabwire@gmail.com>
2024-07-05 17:00:58 +0300

    math lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply 
+let num1=31
+let num2 =2
+multiply = num1 *num2;
 
-// Write your code here
+let random
+random = Math.ceil(Math.random()* 10);
+
+let mod
+let num3=104
+let num4=25
+mod = num3%num4;
+
+let max
+max= Math.max(4,5,16,7,12,20,19,11);
